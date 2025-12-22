adbab57e19304b2b8bfb8ba3df5bb706aa646bdc
pahreeyy <fachrysyarifzu@gmail.com>
2025-12-22 13:41:07 +0700

    add feature

--- api/storage.php
+++ api/storage.php
@@ -1,6 +1,8 @@
 <?php
 require __DIR__ . '/../config.php';
 header('Content-Type: application/json; charset=utf-8');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
 
 // Wajib sudah login
 if (empty($_SESSION['user_id'])) {
